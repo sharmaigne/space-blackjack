4c303b0af85dc796db7a05f8bc69429b51aa1c76
44mira <legolaslada@gmail.com>
2025-01-13 14:56:46 +0800

    fix: remove errors for the meantime

--- main.lua
+++ main.lua
@@ -1,6 +1,3 @@
-local enemies = require("src.entities.enemies")
-local passives = require("src.entities.passives")
-
 function love.load()
 
 end
@@ -10,13 +7,5 @@ function love.update(dt)
 end
 
 function love.draw()
-  for _, e in pairs(enemies) do
-    love.graphics.draw(e.image, 0, 0)
-  end
 
-  for _, p in pairs(passives) do
-    love.graphics.draw(p.image, 0, 250, 0, 0.10, 0.10)
-    love.graphics.print(p.name, 0, 230)
-    love.graphics.print(p.desc, 0, 240, 0, 2, 2)
-  end
 end
